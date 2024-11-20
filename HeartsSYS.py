e9499bca299bd813fd3520df97d8d4676906b5e1
Chiranjeewa Peellawatta <chiranjeewalankeshwara@gmail.com>
2024-11-20 14:20:32 +0530

    updated

--- HeartsSYS.py
+++ HeartsSYS.py
@@ -1,2 +1,10 @@
 import turtle
 from math import pi, sin, cos
+
+def draw_heart(w, h, color):
+    t.pensize(2.5)
+    t.up()
+    t.goto(0, 0)
+    t.down()
+    t.fillcolor(color)
+    t.begin_fill()
